d7b4ae78e40db4620c164c6d43929ccc1ee8731a
VJET Team <vjetteam@avantsoft.com>
2012-02-13 23:26:54 -0800

    fixing test failure

--- core/test/org.ebayopensource.vjet.test.core.jstojava/testFiles/org/ebayopensource/dsf/jst/validation/vjo/BugFixes/Bug8780A.js
+++ core/test/org.ebayopensource.vjet.test.core.jstojava/testFiles/org/ebayopensource/dsf/jst/validation/vjo/BugFixes/Bug8780A.js
@@ -1,6 +1,7 @@
 vjo.ctype("org.ebayopensource.dsf.jst.validation.vjo.BugFixes.Bug8780A")
 .inherits("org.ebayopensource.dsf.jst.validation.vjo.BugFixes.Bug8780")
 .protos({
+		//>public boolean func1(String str) 
         func1 : function(str){
                 return false;
         }
