5b2ed48a280cc7dab8087833137d14d42f2a6b83
VJET Team <vjetteam@avantsoft.com>
2012-03-02 13:05:13 -0800

    Adding otype resolver for object literal field names. Provisional will be changed

---
+++ core/org.ebayopensource.vjet.core.jstojava/src/org/ebayopensource/dsf/jstojava/resolver/IOTypeResolver.java
@@ -0,0 +1,8 @@
+package org.ebayopensource.dsf.jstojava.resolver;
+
+import org.ebayopensource.dsf.jst.term.NV;
+
+public interface IOTypeResolver {
+	String resolve(NV field);
+	String getGroupId();
+}
