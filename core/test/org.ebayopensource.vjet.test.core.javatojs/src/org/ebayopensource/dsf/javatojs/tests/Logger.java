82e900af3bfdcde4aca97a94981b6d250f9aff59
Justin Early <earlyster@gmail.com>
2012-08-16 07:58:56 -0700

    adding Logger class for tests

---
+++ core/test/org.ebayopensource.vjet.test.core.javatojs/src/org/ebayopensource/dsf/javatojs/tests/Logger.java
@@ -0,0 +1,15 @@
+package org.ebayopensource.dsf.javatojs.tests;
+
+public class Logger {
+
+	public static Logger getInstance(Class class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void log(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
